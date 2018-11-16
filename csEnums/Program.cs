604b234f9dacc0d5a3ddd4888a973a6ce20d6af1
Dirk Dobson <dirkvancouver@live.com>
2018-11-16 15:28:34 -0700

    inial commit

---
+++ csEnums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csEnums
+{
+    public enum ShippingMethod 
+    {
+        RegularAirMail,
+        RegisteredAirMail,
+        Express
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
